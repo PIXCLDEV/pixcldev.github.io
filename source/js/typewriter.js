87b004060636648107e3f454265c83b7c9e83b7a
CL <elias.broschin@outlook.de>
2024-11-17 19:52:55 +0100

    feat: add itch.io social

--- source/js/typewriter.js
+++ source/js/typewriter.js
@@ -1,16 +1,39 @@
+//function typewriterEffect(element, text, speed = 100) {
+//    let index = 0;
+
+//    // Set the blinking animation for the element itself
+//    element.style.animation = 'blink 1.3s steps(1) infinite';
+
+//    function type() {
+//        if (index < text.length) {
+//            element.textContent += text.charAt(index);
+//            index++;
+//            setTimeout(type, speed);
+//        }
+//    }
+
+//    type();
+//}
+
 function typewriterEffect(element, text, speed = 100) {
     let index = 0;
 
     // Set the blinking animation for the element itself
     element.style.animation = 'blink 1.3s steps(1) infinite';
 
-    function type() {
-        if (index < text.length) {
-            element.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, speed);
+    // Parse the HTML content
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = text;
+    const textArray = Array.from(tempDiv.childNodes);
+
+        function type() {
+            if (index < text.length) {
+                element.textContent += text.charAt(index);
+
+                index++;
+                setTimeout(type, speed);
+            }
         }
-    }
 
     type();
 }
