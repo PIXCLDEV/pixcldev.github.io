a78106cc007cab9a9dd90f22ebd54215598bf0a7
CL <elias.broschin@outlook.de>
2024-11-17 18:19:01 +0100

    feat: add basic layout and animations

---
+++ source/js/typewriter.js
@@ -0,0 +1,23 @@
+function typewriterEffect(element, text, speed = 100) {
+    let index = 0;
+
+    // Set the blinking animation for the element itself
+    element.style.animation = 'blink 1.3s steps(1) infinite';
+
+    function type() {
+        if (index < text.length) {
+            element.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+}
+
+// Call the function in the external script
+window.addEventListener("DOMContentLoaded", () => {
+    const typewriterElement = document.getElementById('typewriter');
+    const text = "Hey there! I'm PIXCLDEV, how can I help you?";
+    typewriterEffect(typewriterElement, text, 70);
+});
